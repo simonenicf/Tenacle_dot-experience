b7c5b8405a17d327796573c334395d71457a27bb
Michelle <j.s.frankfort@gmail.com>
2022-01-19 09:41:56 +0100

    Added UI Elements & started game over menu

--- tentoontacle_project/Assets/scripts/GameManager.cs
+++ tentoontacle_project/Assets/scripts/GameManager.cs
@@ -4,46 +4,80 @@ using System.Collections.Generic;
 using Leap.Unity;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour
 {
-
-    public GameObject cube;
-    [SerializeField] private GameObject myCube;
-    private GameObject _createdCube;
+    // Tentacle Variable's
     public Transform[] spawnLocations;
+    public GameObject myTentacle;
+    private float spawnTimer;
+    private float spawnCooldown = 1f;
+    public GameObject spawnMyTentacle;
 
-    public ObjectPool pool { get; set; }
+    // score Variable's
+    private int randomSpawnNumber;
+    private int score;
+    [SerializeField] private Text scoreText;
+    
+    // timer variable's
+    [SerializeField] private Text timerText;
+    private float startTimer = 30;
+    private float _currentTimer;
+    
+
+    public int Score
+    {
+        get { return score; }
+        set { score = value; }
+
+    }
+
+    public ObjectPool Pool { get; set; }
 
     private void Awake()
     {
-        myCube = (GameObject) AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/Cube.prefab");
+        myTentacle = (GameObject) AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/Tentacle.prefab");
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _currentTimer = startTimer;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_currentTimer >= 0)
+        {
+            _currentTimer -= 1 * Time.deltaTime;
+            timerText.text = "Timer: " + _currentTimer.ToString("0");
+        }
+        scoreText.text = String.Format("Score: <color=lime>{0}</color>", score);
+        spawnTimer += Time.deltaTime;
+        if (spawnTimer >= spawnCooldown)
+        {SpawnTentacle();}
     }
-
+    
+    
     public void SpawnTentacle()
     {
-        Tentacle tentacle = pool.
+            randomSpawnNumber = Random.Range(0, 9);
+            spawnMyTentacle = Instantiate(myTentacle, spawnLocations[randomSpawnNumber].transform.position, Quaternion.Euler(-45,0,0));
+            spawnTimer = 0;
     }
 
-    public void SummonCube()
+    public void GameOver()
     {
-        _createdCube = Instantiate(myCube);
+        
     }
-
-    public void DestroyCube()
+    
+    public void Restart()
     {
-        Destroy(_createdCube);
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
--- tentoontacle_project/Assets/scripts/Tentacle.cs
+++ tentoontacle_project/Assets/scripts/Tentacle.cs
@@ -1,18 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Leap.Unity.Interaction;
 using UnityEngine;
 
 public class Tentacle : MonoBehaviour
 {
+    [SerializeField] private GameManager gmManager;
+
+    private float selfDestructionTime = 2f;
+    private float howLongAmIOnScreen;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        gmManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        howLongAmIOnScreen += 1 * Time.deltaTime;
+        if (howLongAmIOnScreen >= selfDestructionTime)
+        {
+            Destroy(gameObject);
+            howLongAmIOnScreen = 0;
+        }
+    }
+    
+
+    public void TentacleHit()
+    {
+        print("I got hit");
+        Destroy(gameObject);
+        gmManager.Score += 1;
     }
 }
